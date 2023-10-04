8bc6415298e6daf32092227859eb1b07f3486cda
jakkrapon3236 <631320120@live.kpru.ac.th>
2023-10-05 00:30:14 +0700

    delete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,69 +3,70 @@
 import Image from "next/image";
 import Navbar from "./components/navbar";
 import { useState } from "react";
-import React from "react"; 
+import React from "react";
 
 export default function Home() {
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
-  const [prediction,setPrediction] = useState(0)
+  const [prediction, setPrediction] = useState(0);
   const [uploadStatus, setUploadStatus] = useState(0);
   const [previewImage, setPreviewImage] = useState<string | null>(null);
-  
 
-  function getCookie(name : string) {
-    const cookieValue = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
-    return cookieValue ? cookieValue.pop() : '';
+  function getCookie(name: string) {
+    const cookieValue = document.cookie.match(
+      "(^|;)\\s*" + name + "\\s*=\\s*([^;]+)"
+    );
+    return cookieValue ? cookieValue.pop() : "";
   }
-  
-  const csrftoken = getCookie('csrftoken');
+
+  const csrftoken = getCookie("csrftoken");
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0]; // Use optional chaining here
-  
+
     if (file) {
       const imageUrl = URL.createObjectURL(file);
       setSelectedImage(file);
       setPreviewImage(imageUrl);
     }
   };
-  
-  const headers: { 'X-CSRFToken'?: string } = {
-    'X-CSRFToken': csrftoken, // หรือจะเป็น undefined ก็ได้
+
+  const headers: { "X-CSRFToken"?: string } = {
+    "X-CSRFToken": csrftoken, // หรือจะเป็น undefined ก็ได้
   };
   const looptime = () => {
-    for(let i=0;i<=5;i++){
-      setPrediction(i)
+    for (let i = 0; i <= 5; i++) {
+      setPrediction(i);
     }
-  }
+  };
 
   const handleSubmit = (event: React.FormEvent) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  if (selectedImage !== null) {
-    setUploadStatus(1);
-    const formData = new FormData();
-    formData.append('image', selectedImage);
+    if (selectedImage !== null) {
+      setUploadStatus(1);
+      const formData = new FormData();
+      formData.append("image", selectedImage);
 
-    fetch('http://127.0.0.1:8000/upload/', {
-      method: 'POST',
-      headers: headers,
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
-        setUploadStatus(2);
-        setPrediction(response.prediction);
-        console.log(prediction);
+      fetch("http://127.0.0.1:8000/upload/", {
+        method: "POST",
+        headers: headers,
+        body: formData,
       })
-      .catch((error) => {
-        console.error(error);
-        setUploadStatus(2);
-      });
-  } else {
-    console.error('No image selected.'); // Handle this case as needed.
-  }
-};
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
+          setUploadStatus(2);
+          setPrediction(response.prediction);
+          console.log(prediction);
+        })
+        .catch((error) => {
+          console.error(error);
+          setUploadStatus(2);
+        });
+    } else {
+      console.error("No image selected."); // Handle this case as needed.
+    }
+  };
 
   return (
     <>
@@ -74,33 +75,16 @@ export default function Home() {
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
 
         <div className=" ">
-        
           <div className="grid grid-cols-2  ">
             <div className="flex items-center justify-center">
-              
-              {
-          previewImage == null ? <p>Plase Select Image</p> :
-          <label
-          htmlFor="imageInput"
-          className="border-dashed border-2 border-indigo-600 py-3 px-6 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-          >
-          Cheng image
-          <input
-            type="file"
-            id="imageInput"
-            accept="image/*"
-            style={{ display: "none" }}
-            onChange={handleImageChange}
-          />
-        </label> 
-        }
-         <div className="block max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow">
-                { selectedImage == null ? 
+              {previewImage == null ? (
+                <p>Plase Select Image</p>
+              ) : (
                 <label
                   htmlFor="imageInput"
                   className="border-dashed border-2 border-indigo-600 py-3 px-6 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                 >
-                  Choose an Image
+                  Cheng image
                   <input
                     type="file"
                     id="imageInput"
@@ -108,88 +92,121 @@ export default function Home() {
                     style={{ display: "none" }}
                     onChange={handleImageChange}
                   />
-                </label> : <img
-                  src={previewImage}
-                  alt="Selected Image"
-                  style={{ maxWidth: "100%" }}
-                />}
+                </label>
+              )}
+              <div className="block max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow">
+                {selectedImage == null ? (
+                  <label
+                    htmlFor="imageInput"
+                    className="border-dashed border-2 border-indigo-600 py-3 px-6 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                  >
+                    Choose an Image
+                    <input
+                      type="file"
+                      id="imageInput"
+                      accept="image/*"
+                      style={{ display: "none" }}
+                      onChange={handleImageChange}
+                    />
+                  </label>
+                ) : (
+                  <img
+                    src={previewImage ?? undefined}
+                    alt="Selected Image"
+                    style={{ maxWidth: "100%" }}
+                  />
+                )}
               </div>
 
               <div>
-              {
-          selectedImage == null ? <p></p> :<div><label htmlFor="imageInput" className="item-center">
-          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={handleSubmit}>Prediction</button>
-        </label> </div>
-        }
+                {selectedImage == null ? (
+                  <p></p>
+                ) : (
+                  <div>
+                    <label htmlFor="imageInput" className="item-center">
+                      <button
+                        type="button"
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        onClick={handleSubmit}
+                      >
+                        Prediction
+                      </button>
+                    </label>{" "}
+                  </div>
+                )}
               </div>
-              
-             
-              
             </div>
-            
+
             <div className="flex items-center justify-center row-span-3">
-           {
-            uploadStatus == 0 ? 
-            <div>
-            <label className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-Waiting for response
-</span>
-</label>
+              {uploadStatus == 0 ? (
+                <div>
+                  <label className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                      Waiting for response
+                    </span>
+                  </label>
+                </div>
+              ) : uploadStatus == 1 ? (
+                <div role="status">
+                  <svg
+                    aria-hidden="true"
+                    className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-green-500"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  <span className="sr-only">Loading...</span>
+                </div>
+              ) : (
+                <div className="grid grid-rows-3">
+                  <div>
+                    <label className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                      <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        {(prediction[0] * 100).toFixed(2)}
+                      </span>
+                    </label>
+                  </div>
+                  <div>
+                    <label className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                      <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        {(prediction[1] * 100).toFixed(2)}
+                      </span>
+                    </label>
+                  </div>
+                  <div>
+                    <label className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                      <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        {(prediction[2] * 100).toFixed(2)}
+                      </span>
+                    </label>
+                  </div>
+                  <div>
+                    <label className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                      <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        {(prediction[3] * 100).toFixed(2)}
+                      </span>
+                    </label>
+                  </div>
+                  <div>
+                    <label className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                      <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        {(prediction[4] * 100).toFixed(2)}
+                      </span>
+                    </label>
+                  </div>
+                </div>
+              )}
             </div>
-
-            : uploadStatus == 1 ? 
-            <div role="status">
-            <svg aria-hidden="true" className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-green-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-            </svg>
-            <span className="sr-only">Loading...</span>
-        </div>
-            :
-           
-            <div className="grid grid-rows-3">
-              <div>
-              <label className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-{(prediction[0] * 100).toFixed(2)}
-</span>
-</label>
-              </div>
-              <div>
-              <label className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-{(prediction[1] * 100).toFixed(2)}
-</span>
-</label>
-              </div>
-              <div>
-              <label className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-{(prediction[2] * 100).toFixed(2)}
-</span>
-</label>
-              </div>
-              <div>
-              <label className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-{(prediction[3] * 100).toFixed(2)}
-</span>
-</label>
-              </div>
-              <div>
-              <label className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-{(prediction[4] * 100).toFixed(2)}
-</span>
-</label>
-              </div>
-              </div>
-          
-
-           }
-           </div>
-                     </div>
+          </div>
         </div>
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
       </main>
